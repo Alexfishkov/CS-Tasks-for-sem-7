df954f2dc79a64fe615a99b1d2eee6feb6cba417
Alexfishkov <afscorp@mail.ru>
2022-12-23 15:54:22 +0300

    Presence of the element revised

--- Task 50/Program.cs
+++ Task 50/Program.cs
@@ -16,18 +16,18 @@ void FillArray (int[,] array)
     }
 }
 
-// void PrintArray(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write($"{array[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-//     Console.WriteLine();
-// }
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
 
 int lines = new Random().Next(2, 11);
 int columns = new Random().Next(2, 11);
@@ -37,14 +37,14 @@ int[,] ourArray = new int[lines, columns];
 FillArray(ourArray);
 
 Console.Clear();
-// PrintArray(ourArray);
+PrintArray(ourArray);
 Console.WriteLine("Определяем есть ли в заданном массиве элемент с указаными вами позициями");
 Console.Write("Введите номер строки (от 0 до 10)=> ");
 int nLine = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите номер столбца (от 0 до 10)=> ");
 int nColumn = Convert.ToInt32(Console.ReadLine());
 
-if (nColumn <= columns && nLine <= lines)
+if (nColumn < columns && nLine < lines)
 {
     Console.WriteLine($"Такой элемент есть в массиве, его значение: {ourArray[nLine, nColumn]}");
 }
